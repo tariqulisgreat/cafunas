ecc5ad6046fa4eaa1ef17db38b12cfc341be217f
jkassociates <jkassociatesinfo@gmail.com>
2025-10-05 17:04:01 +0600

    aa

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/cafunas/', // 👈 Must match GitHub repo name exactly
   plugins: [react()],
 })
